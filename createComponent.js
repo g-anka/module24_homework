ff5c638f1c754d07204bff482db721628297c0b8
g-anka <76132583+g-anka@users.noreply.github.com>
2021-09-25 22:43:35 +0300

    Update createComponent.js

--- createComponent.js
+++ createComponent.js
@@ -7,20 +7,28 @@ const successChalk = chalk.green;
 const errorChalk = chalk.red.bold;
 const warningChalk = chalk.yellow.bgGray;
 
+let defaultComponent = "";
+let variant = "";
+
 const argv = yargs
     .command(['create <filename> [filepath]', 'c'], 'Creates React Component template', {
         functional: {
             alias: "f",
             boolean: true
+        },
+        
+        class: {
+            alias: "s",
+            boolean: true
         }
-    }, (argv) => {
-        if(argv.functional) {
-            console.log(successChalk("Functional Component starts"))
-        }
+    }, (argv) => {        
         const defaultPath = `./src/components/${argv.filename}.js`;
         const thePath = path.resolve((argv.filepath) ? argv.filepath : defaultPath);
-        const defaultFuncComponent =
-`import React from "react";
+
+        if(argv.functional) {
+            variant = "functional";
+            defaultComponent =
+                `import React from "react";
             
 function ${argv.filename}() {
     return (
@@ -31,9 +39,26 @@ function ${argv.filename}() {
 }
             
 export default ${argv.filename};`;
+        } else if (argv.class) {
+            variant = "class";
+            defaultComponent =
+                `import React from "react";
+            
+class ${argv.filename} extends React.Component {
+    render() {
+        return (
+            <div>
+                Some content
+            </div>
+        );
+    }
+}     
+     
+export default ${argv.filename};`;
+        }
 
-        fs.writeFile(thePath, defaultFuncComponent)
-            .then(() => console.log(successChalk("Your Component has been successfully created")))
+        fs.writeFile(thePath, defaultComponent)
+            .then(() => console.log(successChalk(`Your ${variant} Component has been successfully created`)))
             .catch(e => console.log(errorChalk("An error has occurred:", e)));
     })
     .demandCommand(1, (warningChalk("At least 1 command should be passed")))
